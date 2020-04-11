7dd84983ad51de84b75a5a023ed5c610a3d2265c
jesusemen <62002596+jesusemen@users.noreply.github.com>
2020-04-11 04:34:18 -0700

    Javascript Task 1

---
+++ courses.js
@@ -0,0 +1,16 @@
+
+ let myName = 'Jesusemen';
+ let myCourses = [ 'frontend =>javascript', 'mobile => flutter', 'design => uiux'];
+  console.log(myName);
+  console.log(myCourses);
+
+numCourses = myCourses.length;
+console.log("No of courses is" + " " +  numCourses + " " +  "which is odd");
+
+   for( let i = 0; i < 200; i++){
+if (i % 2 !== 0){
+
+    console.log(i);
+}
+        
+   }
